5c2097262a0c0f29e95586924182c02fcbc9ae30
Arman Chatikyan <chatikyana@gmail.com>
2018-10-25 14:52:49 +0400

    Initial message

---
+++ app/src/main/java/io/armcha/sampleapp/MainActivity.kt
@@ -0,0 +1,18 @@
+package io.armcha.sampleapp
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        myView.setOnClickListener {
+            Log.e("AA","AAAAAA")
+        }
+    }
+}
---
+++ app/src/test/java/io/armcha/sampleapp/ExampleUnitTest.kt
@@ -0,0 +1,22 @@
+package io.armcha.sampleapp
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+
+    fun `this is test case for`(){
+
+    }
+
+}
